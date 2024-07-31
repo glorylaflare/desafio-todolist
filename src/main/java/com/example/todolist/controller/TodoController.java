68687e4288a313ec27fa18556ddcc809354d9cdb
Marcelo Gonçalves <juninho_toh@hotmail.com>
2024-07-31 13:26:09 -0300

    feat: create Todo controller

---
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -0,0 +1,36 @@
+package com.example.todolist.controller;
+
+import com.example.todolist.domain.Todo;
+import com.example.todolist.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    @Autowired
+    private TodoService todoService;
+
+    @PostMapping
+    public List<Todo> createTodo(@RequestBody Todo todo) {
+        return todoService.createToDo(todo);
+    }
+
+    @GetMapping
+    public List<Todo> listAllTodo() {
+        return todoService.listAll();
+    }
+
+    @PutMapping
+    public List<Todo> updateTodo(@RequestBody Todo todo) {
+        return todoService.updateToDo(todo);
+    }
+
+    @DeleteMapping("{id}")
+    public List<Todo> deleteTodo(@PathVariable Long id) {
+        return todoService.deleteTodo(id);
+    }
+}
