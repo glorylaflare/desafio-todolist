07f5b0fdc0856df9a96fd0679c47cad7859ede18
Marcelo Gonçalves <juninho_toh@hotmail.com>
2024-07-31 14:18:34 -0300

    feat: add constructor Todo and annotations @NotBlank

--- src/main/java/com/example/todolist/domain/Todo.java
+++ src/main/java/com/example/todolist/domain/Todo.java
@@ -1,6 +1,7 @@
 package com.example.todolist.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
@@ -11,8 +12,17 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String nome;
+    @NotBlank
     private String descricao;
     private boolean realizado;
     private int prioridade;
+
+    public Todo(String nome, String descricao, boolean realizado, int prioridade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.realizado = realizado;
+        this.prioridade = prioridade;
+    }
 }
